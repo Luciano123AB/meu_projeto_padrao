9c4afbce74d527bf9430b10563ae300097c960f8
Luciano Eduardo <luciano123ab@gmail.com>
2025-07-19 19:59:33 -0300

    001(Login)

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,9 @@
 <?php
 
+use App\Http\Controllers\MainController;
+use App\Http\Middleware\VerificarNaoEstaLogado;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware([VerificarNaoEstaLogado::class])->group(function () {
+    Route::get("/login", [MainController::class, "login"])->name("login");
+});
