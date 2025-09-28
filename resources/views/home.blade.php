05f46515d566c55d42cd05f70dc4e0fc6ebed769
Luciano Eduardo <luciano123ab@gmail.com>
2025-09-28 19:11:45 -0300

    013(Responsividade_Melhorias)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
             <div class="col-12 col-md-10 py-3" style="min-height: 700px">
                 <div class="row text-center text-md-start align-items-center">
                     <div class="col-12 col-md-6 text-center mb-4 mb-md-0">
-                        <h1 class="fs-3 fs-md-4 fs-lg-5">Seja BEM VINDO ao Nosso Site!</h1>
+                        <h1 class="fs-md-4 fs-lg-5">Seja BEM VINDO ao Nosso Site!</h1>
                     </div>
 
                     <div class="col-12 col-md-6 text-center">
@@ -23,6 +23,8 @@
                         <p class="{{ session("tema") == "escuro" ? "text-white" : "text-black" }}">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi beatae odio labore, corporis in debitis? Tempore tempora necessitatibus, libero, dolorum maiores autem molestias officia natus sunt ab, facilis suscipit accusantium.</p>
                         
                         <p class="{{ session("tema") == "escuro" ? "text-white" : "text-black" }}">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus sint quae delectus molestias deleniti, dignissimos qui quam voluptatem laborum sit, recusandae ab fugit mollitia porro. Vitae unde ad magni aspernatur.</p>
+                    
+                        <p class="{{ session("tema") == "escuro" ? "text-white" : "text-black" }}">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, fugit totam necessitatibus ab dicta laudantium quibusdam deleniti dolorum accusamus cum voluptatem nobis architecto exercitationem optio id blanditiis placeat. Neque, exercitationem.</p>
                     </div>
                 </div>
             </div>
