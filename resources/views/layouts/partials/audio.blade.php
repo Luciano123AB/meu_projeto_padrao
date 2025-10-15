04f43a05145951ec51c6aa17de6ace8f54b428dd
Luciano Eduardo <luciano123ab@gmail.com>
2025-10-15 20:11:16 -0300

    014(Responsividade_Melhorias)

--- resources/views/layouts/partials/audio.blade.php
+++ resources/views/layouts/partials/audio.blade.php
@@ -3,7 +3,7 @@
 </audio>
 
 <a href="{{ route("tocarMusica") }}" id="musica" class="btn {{ session("tema") == "escuro" ? "btn-secondary" : "btn-info" }} border shadow rounded-circle px-3 py-2">
-    <i class="bi {{ session("musica") == "desativado" ? "bi-volume-mute" : "bi-volume-up" }} fs-4"></i>
+    <i class="bi {{ session("musica") == "desativado" ? "bi-volume-up" : "bi-volume-mute" }} fs-4"></i>
 </a>
 
 <script>
