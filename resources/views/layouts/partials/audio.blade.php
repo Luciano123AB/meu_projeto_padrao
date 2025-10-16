426e660ea9d4db157b439acab62648378629ba26
Luciano Eduardo <luciano123ab@gmail.com>
2025-10-15 21:34:58 -0300

    014(Responsividade_Melhorias)

--- resources/views/layouts/partials/audio.blade.php
+++ resources/views/layouts/partials/audio.blade.php
@@ -1,9 +1,9 @@
 <audio id="trilha_sonora" autoplay muted loop>
-    <source src="{{ asset("storage/audios/trilha_sonora.mp3") }}" type="audio/mpeg">
+    <source src="{{ asset("assets/audios/trilha_sonora.mp3") }}" type="audio/mpeg">
 </audio>
 
 <a href="{{ route("tocarMusica") }}" id="musica" class="btn {{ session("tema") == "escuro" ? "btn-secondary" : "btn-info" }} border shadow rounded-circle px-3 py-2">
-    <i class="bi {{ session("musica") == "desativado" ? "bi-volume-up" : "bi-volume-mute" }} fs-4"></i>
+    <i class="bi {{ session("musica") == "desativado" ? "bi-volume-mute" : "bi-volume-up" }} fs-4"></i>
 </a>
 
 <script>
