9ed728ef231f6ed63eb16580044c643eaa87c710
Luciano Eduardo <luciano123ab@gmail.com>
2026-06-24 11:37:33 -0300

    016(Testes_Melhorias)

--- database/factories/UsuarioFactory.php
+++ database/factories/UsuarioFactory.php
@@ -19,30 +19,14 @@ class UsuarioFactory extends Factory
      */
     public function definition(): array
     {
-
-        $cpf = $this->faker->randomNumber(2);
-        $celular = $this->faker->randomNumber(4);
-
-        if ($cpf <= 9) {
-            $cpf = '0' . $cpf;
-        }
-
-        if ($celular <= 9) {
-            $celular = '999' . $celular;
-        } else if ($celular <= 99) {
-            $celular = '99' . $celular;
-        } else if ($celular <= 999) {
-            $celular = '9' . $celular;
-        }
-
         return [
             'nome_completo' => $this->faker->name(),
             'usuario' => $this->faker->userName(),
             'email' => $this->faker->email(),
             'senha' => Hash::make('@24032004ABcd123'),
-            'cpf' => '123.456.789-' . $cpf,
+            'cpf' => '123.456.789-' . $this->faker->unique()->numberBetween(0, 99),
             'data_nascimento' => $this->faker->date(),
-            'celular' => '(55)99999-' . $celular,
+            'celular' => '(55)99999-' . $this->faker->unique()->numberBetween(0, 9999),
             'genero' => $this->faker->randomElement([
                 'Masculino',
                 'Feminino',
